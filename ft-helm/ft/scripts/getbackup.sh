3d5952a49a209605aeede451ead2071ad8cfbae8
Julien Aubert <j.aubert@actongroup.com>
2025-03-25 09:53:11 +0100

    restore

--- ft-helm/ft/scripts/getbackup.sh
+++ ft-helm/ft/scripts/getbackup.sh
@@ -3,6 +3,7 @@
 backup_folder="/backup"
 rm -rf $backup_folder
 mkdir -p $backup_folder/backup
+mkdir -p $backup_folder/data
 echo "download redis dump from s3"
 aws --no-verify-ssl s3 cp s3://$BACKUP_BUCKET_NAME/redis_backup_${BACKUP_DATE}.zip $backup_folder/redis-dump.zip --endpoint-url $STORAGE_ENDPOINT
 echo "finish download"
