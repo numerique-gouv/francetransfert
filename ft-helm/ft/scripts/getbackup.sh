ae2d1f9f8d316044e697ed4e63b4e20f588c5deb
Julien Aubert <j.aubert@actongroup.com>
2025-03-25 09:40:03 +0100

    restore

--- ft-helm/ft/scripts/getbackup.sh
+++ ft-helm/ft/scripts/getbackup.sh
@@ -4,16 +4,18 @@ backup_folder="/backup"
 rm -rf $backup_folder
 mkdir -p $backup_folder/backup
 echo "download redis dump from s3"
-aws --no-verify-ssl s3 cp s3://$BACKUP_BUCKET_NAME/redis-dump-${BACKUP_DATE}.tar.gz $backup_folder/redis-dump.tar.gz --endpoint-url $STORAGE_ENDPOINT
+aws --no-verify-ssl s3 cp s3://$BACKUP_BUCKET_NAME/redis_backup_${BACKUP_DATE}.zip $backup_folder/redis-dump.zip --endpoint-url $STORAGE_ENDPOINT
 echo "finish download"
 
 echo "extract redis dump"
 
-if [ -f "$backup_folder/redis-dump.tar.gz" ]; then
-    tar -xzf $backup_folder/redis-dump.tar.gz -C $backup_folder/backup
+if [ -f "$backup_folder/redis-dump.zip" ]; then
+    echo `ls -al $backup_folder/**`
+    unzip $backup_folder/redis-dump.zip -d $backup_folder/backup
+    echo `ls -al $backup_folder/**`
     mv $backup_folder/backup/backup $backup_folder/backup/data
     echo "finish extract"
 else
-    echo "redis-dump.tar.gz not found"
+    echo "redis-dump.zip not found"
     exit 1
 fi
