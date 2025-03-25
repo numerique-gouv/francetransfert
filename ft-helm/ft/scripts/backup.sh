3faabb30d91b338a7fefa8d38a99d39379f240e6
Julien Aubert <j.aubert@actongroup.com>
2025-03-25 09:33:44 +0100

    backup

--- ft-helm/ft/scripts/backup.sh
+++ ft-helm/ft/scripts/backup.sh
@@ -8,11 +8,13 @@ month=$(date +"%m")
 backup_folder="/backup"
 
 echo "compress backup folder"
-tar cfz $backup_folder/redis-dump.tar.gz $backup_folder
+zip -r $backup_folder/redis-dump.zip $backup_folder
 echo "upload redis dump to s3"
-aws --no-verify-ssl s3 cp $backup_folder/redis-dump.tar.gz s3://$BACKUP_BUCKET_NAME/redis-dump-${now}.tar.gz --endpoint-url $STORAGE_ENDPOINT
+aws --no-verify-ssl s3 cp $backup_folder/redis-dump.zip s3://$BACKUP_BUCKET_NAME/redis_backup_${now}.zip --endpoint-url $STORAGE_ENDPOINT
 if [ $? -ne 0 ]; then
   echo "failed to upload redis dump to s3"
   exit 1
 fi
+echo "clean old redis dump in s3"
+aws --no-verify-ssl s3api list-objects --endpoint-url $STORAGE_ENDPOINT --bucket $REDIS_BUCKET --query 'Contents[?LastModified<=`'$(date -d '7 days ago' +%Y-%m-%d)`'].{Key:Key}' --output text | xargs -I {} aws s3 rm s3://$REDIS_BUCKET/{} --endpoint-url $STORAGE_ENDPOINT
 echo "finish backup"
