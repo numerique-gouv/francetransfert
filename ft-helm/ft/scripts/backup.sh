bdc087c73c97734497d1c55a686ed3e021d7b935
Julien Aubert <j.aubert@actongroup.com>
2025-03-25 16:31:25 +0100

    cleanold

--- ft-helm/ft/scripts/backup.sh
+++ ft-helm/ft/scripts/backup.sh
@@ -16,7 +16,7 @@ if [ $? -ne 0 ]; then
   echo "failed to upload redis dump to s3"
   exit 1
 fi
-delete_date=$(date -d '7 days ago' +%Y-%m-%d)
+delete_date=$(date -d @$(($(date +%s) - 86400 * 7)) +%Y-%m-%d)
 echo "clean old redis dump in s3"
-aws --no-verify-ssl s3api list-objects --endpoint-url $STORAGE_ENDPOINT --bucket $REDIS_BUCKET --query 'Contents[?LastModified<='$delete_date'].{Key:Key}' --output text | xargs -I {} aws s3 rm s3://$REDIS_BUCKET/{} --endpoint-url $STORAGE_ENDPOINT
+aws --no-verify-ssl s3api list-objects --endpoint-url $STORAGE_ENDPOINT --bucket $BACKUP_BUCKET_NAME --query 'Contents[?LastModified<=`'$delete_date'`].{Key:Key}' --output text | xargs -I {} aws s3 rm s3://$BACKUP_BUCKET_NAME/{} --endpoint-url $STORAGE_ENDPOINT
 echo "finish backup"
