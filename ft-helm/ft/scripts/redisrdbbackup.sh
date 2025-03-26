6df48fb412cfa0539bbd919e0f7f2a0f36565cd0
Julien Aubert <j.aubert@actongroup.com>
2025-03-26 16:28:48 +0100

    check using redis-cli

--- ft-helm/ft/scripts/redisrdbbackup.sh
+++ ft-helm/ft/scripts/redisrdbbackup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+mkdir -p /backup/backup-redis
+
 redis-cli -h "$FT_REDIS_SERVICE_HOST" -p "$FT_REDIS_SERVICE_PORT_TCP_REDIS" -a "$METALOAD_PASSWORD" --rdb "/backup/backup-redis/ftr-data.rdb"
 # check if backup folder is empty
 if [ -z "$(ls /backup/backup-redis)" ]; then
