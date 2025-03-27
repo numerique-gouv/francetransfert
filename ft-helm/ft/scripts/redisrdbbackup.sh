289be296eba7bf0139081ba3b884947e0d3ca9f1
Julien Aubert <j.aubert@actongroup.com>
2025-03-27 08:41:24 +0100

    test backup

--- ft-helm/ft/scripts/redisrdbbackup.sh
+++ ft-helm/ft/scripts/redisrdbbackup.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 mkdir -p /backup/backup-redis
+mkdir -p /data
 
-redis-cli -h "$FT_REDIS_SERVICE_HOST" -p "$FT_REDIS_SERVICE_PORT_TCP_REDIS" -a "$METALOAD_PASSWORD" --rdb "/backup/backup-redis/ftr-data.rdb"
+MASTERS_IP=($(redis-cli -h "$FT_REDIS_SERVICE_HOST" -p "$FT_REDIS_SERVICE_PORT_TCP_REDIS" -a "$METALOAD_PASSWORD" --no-auth-warning cluster nodes | grep "master" | awk '{print $2}' | cut -d "@" -f1))
+
+echo "master ip $MASTERS_IP"
+redis-cli -h "$FT_REDIS_SERVICE_HOST" -p "$FT_REDIS_SERVICE_PORT_TCP_REDIS" -a "$METALOAD_PASSWORD" --rdb "/data/ftr-data.rdb"
+redis-cli -h "$FT_REDIS_SERVICE_HOST" -p "$FT_REDIS_SERVICE_PORT_TCP_REDIS" -a "$METALOAD_PASSWORD" --rdb "ftr-data.rdb"
 # check if backup folder is empty
 if [ -z "$(ls /backup/backup-redis)" ]; then
   echo "backup folder is empty"
