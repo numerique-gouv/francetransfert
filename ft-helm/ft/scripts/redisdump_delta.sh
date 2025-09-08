da7f4f18735e560738d55e9522cd4f57a46df019
Julien Aubert <j.aubert@actongroup.com>
2025-09-08 18:11:28 +0200

    redis delta backup

--- ft-helm/ft/scripts/redisdump_delta.sh
+++ ft-helm/ft/scripts/redisdump_delta.sh
@@ -3,17 +3,6 @@
 pod=$(kubectl get pods -o=name --field-selector status.phase=Running | grep "redis-node" | sed "s/^.\{4\}//" | head -n 1)
 mkdir -p /backup/backup-redis
 
-echo "BGSAVE"
-kubectl exec $pod -- redis-cli -a "$METALOAD_PASSWORD" BGSAVE
-
-finished=0
-while [ $finished -eq 0 ]; do
-  sleep 2
-  echo "WAIT FOR BGSAVE"
-  kubectl exec $pod -- redis-cli -a "$METALOAD_PASSWORD" INFO PERSISTENCE | grep -q "rdb_bgsave_in_progress:1"
-  finished=$?
-done
-
 echo "copy dump file from $pod to /backup/backup-redis"
 kubectl cp $pod:/data /backup/backup-redis
 ls -lrt /backup/backup-redis
