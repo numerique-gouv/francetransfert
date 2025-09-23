283de687b09d92d3231120071c8e654c5f6bf380
Julien Aubert <j.aubert@actongroup.com>
2025-09-23 22:39:45 +0200

    wait terminaison worker

--- francetransfert-worker/src/main/java/fr/gouv/culture/francetransfert/worker/ScheduledTasks.java
+++ francetransfert-worker/src/main/java/fr/gouv/culture/francetransfert/worker/ScheduledTasks.java
@@ -12,6 +12,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -65,6 +66,9 @@ public class ScheduledTasks {
 
 	@Value("${queue.sleep:1500}")
 	private int queueSleep;
+
+	@Value("${shutdown.seconds:20}")
+	private int shutdownSeconds;
 
 	@Autowired
 	private MailAvailbleEnclosureServices mailAvailbleEnclosureServices;
@@ -576,15 +580,24 @@ public class ScheduledTasks {
 		executorList.forEach(executor -> {
 			try {
 				executor.setQueueCapacity(0);
-				executor.initiateShutdown();
+				executor.shutdown();
+				executor.getThreadPoolExecutor().awaitTermination(shutdownSeconds, TimeUnit.SECONDS);
 			} catch (Exception e) {
 				LOGGER.error("Cannot stop executor ", e);
 			}
 		});
-		WorkerUtils.activeTasks.forEach(task -> {
-			LOGGER.info("Putting back to queue {} - {}", task.getQueue(), task.getData());
-			redisManager.publishFT(task.getQueue(), task.getData());
-		});
+		if (!CollectionUtils.isEmpty(WorkerUtils.activeTasks)) {
+			LOGGER.info("Active tasks found, waiting 10 seconds");
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				LOGGER.error("Error sleep", e);
+			}
+			WorkerUtils.activeTasks.forEach(task -> {
+				LOGGER.info("Putting back to queue {} - {}", task.getQueue(), task.getData());
+				redisManager.publishFT(task.getQueue(), task.getData());
+			});
+		}
 		LOGGER.info("Finished putting task back to queue");
 
 	}
