83aaed18e405abd8e9cc09d42a9b6316b4b2a89b
Julien AUBERT <j.aubert@actongroup.com>
2026-09-08 14:45:47 +0200

    new version disable iphone

--- francetransfert-upload-download-gui/src/environments/environment.prod.ts
+++ francetransfert-upload-download-gui/src/environments/environment.prod.ts
@@ -53,5 +53,5 @@ export const environment = {
     }
   },
   expiredLimit: 60,
-  version: '4.0.9'
+  version: '4.0.10'
 };
--- francetransfert-upload-download-gui/src/main.ts
+++ francetransfert-upload-download-gui/src/main.ts
@@ -10,10 +10,34 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { isIOS } from './app/shared/is-ios';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+async function unregisterServiceWorkerOnIOS(): Promise<void> {
+  if (!isIOS() || !('serviceWorker' in navigator)) {
+    return;
+  }
+
+  const registrations = await navigator.serviceWorker.getRegistrations();
+  if (registrations.length > 0) {
+    await Promise.all(registrations.map(registration => registration.unregister()));
+  }
+
+  if ('caches' in window) {
+    const keys = await caches.keys();
+    if (keys.length > 0) {
+      await Promise.all(keys.map(key => caches.delete(key)));
+    }
+  }
+}
+
+unregisterServiceWorkerOnIOS()
+  .catch(err => console.error('Failed to unregister service worker on iOS', err))
+  .finally(() => {
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+  });
