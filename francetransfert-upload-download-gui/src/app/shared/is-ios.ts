5614b7a9662ae6878fa1d6a7332bc1f80c4c6acb
Julien AUBERT <j.aubert@actongroup.com>
2026-09-08 14:37:19 +0200

    disable iphone

--- francetransfert-upload-download-gui/src/app/shared/is-ios.ts
+++ francetransfert-upload-download-gui/src/app/shared/is-ios.ts
@@ -6,9 +6,10 @@
   */
 
 export function isIOS(): boolean {
+
   if (typeof navigator === 'undefined') {
     return false;
   }
-  return /iPad|iPhone|iPod/.test(navigator.userAgent)
+  return /AppleWebKit/.test(navigator.userAgent)
     || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
 }
